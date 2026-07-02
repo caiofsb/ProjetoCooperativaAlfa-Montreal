f1fdaa5cec3eea238387013327d284fe0d97af44
Caio Felipe <caio.bindeiro@gmail.com>
2026-07-02 19:15:00 -0300

    Criando scripts do db2

---
+++ db/dados-iniciais.sql
@@ -0,0 +1,26 @@
+INSERT INTO CLIENTES
+    (CLI_CODIGO, CLI_NOME, CLI_EMAIL, CLI_TELEFONE)
+VALUES
+    ('000001', 'Ana Silva', 'ana.silva@cooperativa.com', '11988887777');
+
+INSERT INTO CLIENTES
+    (CLI_CODIGO, CLI_NOME, CLI_EMAIL, CLI_TELEFONE)
+VALUES
+    ('000002', 'Bruno Costa', 'bruno.costa@cooperativa.com', '21977776666');
+
+INSERT INTO CLIENTES
+    (CLI_CODIGO, CLI_NOME, CLI_EMAIL, CLI_TELEFONE)
+VALUES
+    ('000003', 'Carla Souza', 'carla.souza@cooperativa.com', NULL);
+
+INSERT INTO CLIENTES
+    (CLI_CODIGO, CLI_NOME, CLI_EMAIL, CLI_TELEFONE)
+VALUES
+    ('000004', 'Diego Lima', 'diego.lima@cooperativa.com', '31966665555');
+
+INSERT INTO CLIENTES
+    (CLI_CODIGO, CLI_NOME, CLI_EMAIL, CLI_TELEFONE)
+VALUES
+    ('000005', 'Elisa Martins', 'elisa.martins@cooperativa.com', NULL);
+
+COMMIT;
